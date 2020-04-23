61a52bb35064dca5b42c6dc623b2ba917389d335
ibabaityte <47790653+ibabaityte@users.noreply.github.com>
2020-04-23 22:11:32 +0300

    Updated the schema date option to timestamps option

--- api/src/models/entry.js
+++ api/src/models/entry.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const entrySchema = new mongoose.Schema({
     title: String,
-    content: String,
-    dateCreated: Date,
-    dateUpdated: Date
+    content: String
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model("Entry", entrySchema);
