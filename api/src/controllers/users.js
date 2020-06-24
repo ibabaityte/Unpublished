cd51095255f3700926805574003403e071d3c777
ibabaityte <47790653+ibabaityte@users.noreply.github.com>
2020-06-25 01:07:26 +0300

    Added user get and remove controller

--- api/src/controllers/users.js
+++ api/src/controllers/users.js
@@ -21,6 +21,45 @@ const register = (req, res) => {
 //
 // };
 
+const get = (req, res) => {
+    Users.findById(req.params.id).then(user =>  {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found"
+            });
+        }
+        res.status(200).send(user);
+    }).catch(err => {
+       res.status(500).send({
+           message: err.message || "An error occurred while retrieving user"
+       });
+    });
+};
+
+// const update = (req, res) => {
+//
+// };
+
+const remove = (req, res) => {
+    Users.findByIdAndRemove(req.params.id).then(user => {
+        if(!user) {
+            return res.status(404).send({
+                message: "User not found"
+            });
+        }
+        res.status(200).send({message: "User profile deleted successfully"});
+    }).catch(err => {
+        if (err.kind === "ObjectId" || err.name === "NotFound") {
+            return res.status(404).send({
+                message: "User not found"
+            });
+        }
+        return res.status(500).send({
+            message: "Could not delete this user profile"
+        });
+    });
+};
+
 module.exports = {
-  register, auth
+  register, get, remove
 };
